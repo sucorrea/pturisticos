8f2004b478cf0a4e66a613d8a0f09fc5d00a12a1
Sueli Correa <oliver.sueli@gmail.com>
2021-05-16 14:27:43 -0300

    First

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <header>
+        <nav class="navbar navbar-dark">
+        <div><a href="#" className="navbar-brand">Inicio</a></div>
+        <div><a href="#" className="navbar-brand">Resultados</a></div>
+        <div><a href="#" className="navbar-brand">Detalhes</a></div>
+
+
+        </nav>
+      </header>
+    )
+}
+export default Header
+
