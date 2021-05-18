b245ea84e6f51023477deca120d34a7def42f7be
Sueli Correa <oliver.sueli@gmail.com>
2021-05-18 11:40:22 -0300

    final

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import AddPoint from "./components/AddPoint";
 import FindPoint from "./components/FindPoint";
 import ShowPoint from "./components/ShowPoint";
 import ViewPoint from "./components/ViewPoint";
-function App() {
+
+const App = () => {
   return (
     <div className="container">
-      <Header />
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/" exact component={FindPoint}></Route>
-            <Route path="/pontos" component={FindPoint}></Route>
-            <Route path="/novo" component={AddPoint}></Route>
-            <Route path="/exibir-ponto/:id" component={ViewPoint}></Route>
-            <Route path="/mostrar-ponto/:nome" component={ShowPoint}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+    <Header />
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={FindPoint}></Route>
+          <Route path="/pontos" component={FindPoint}></Route>
+          <Route path="/novo" component={AddPoint}></Route>
+          <Route path="/exibir-ponto/:id" component={ViewPoint}></Route>
+          <Route path="/mostrar-ponto/:nome" component={ShowPoint}></Route>
+        </Switch>
+      </div>
+    </Router>
+  </div>
+  )
 }
 
-export default App;
+export default App
+
--- src/services/servicePoint.js
+++ src/services/servicePoint.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 
-const PONTOS_API_BASE_URL = "http://localhost:7777/pturisticos";
+const PONTOS_API_BASE_URL = "http://localhost:5000/pturisticos";
 class servicePoint {
 
-    getPontosSemfiltro(filtro) {
+    getPontosSemfiltro(_filtro) {
         return axios.get(PONTOS_API_BASE_URL);
     }
     getPontos(filtro) {
         return axios.get(PONTOS_API_BASE_URL + "/nome/" + filtro);
     }
-    getPontosID(filtroID) {
-        return axios.get(PONTOS_API_BASE_URL + "/id/" + filtroID);
-    }
-
     salvar(ponto) {
         return axios.post(PONTOS_API_BASE_URL, ponto);
 
